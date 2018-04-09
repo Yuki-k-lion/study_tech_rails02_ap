1798888d3c7e60cb20ac778516363649d46996e7
EC2 Default User <ec2-user@ip-172-31-0-56.us-east-2.compute.internal>
2018-04-09 05:41:29 +0000

    add ranking first step

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,3 +1,11 @@
 class RankingController < ApplicationController
   layout 'review_site'
+  
+  before_action :ranking
+  
+  def ranking
+      # @ranking = Product.all.limit(5)
+      # limitメソッドも、orderメソッドと同様、allメソッドを省略することができます。
+        @ranking = Product.limit(5)
+  end
 end
