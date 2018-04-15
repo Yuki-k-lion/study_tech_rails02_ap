61d0d9f2e1d10f1299a2a1d0014325bcd4cf6867
EC2 Default User <ec2-user@ip-172-31-0-56.us-east-2.compute.internal>
2018-04-15 06:04:32 +0000

    setup avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  
+  has_attached_file :avatar,
+                        styles: { medium: "300x300#", thumb: "100x100#" }
+  validates_attachment_content_type :avatar,
+                        content_type: ["image/jpg","image/jpeg","image/png"]
+
+# 100×100	横100px、縦100pxの画像(アスペクト比を保つ)
+# 100×100!	アスペクト比を無視
+# 100×100>	一番長い辺を100pxにするようにリサイズ(アスペクト比を保つ)
+# 100×100^	一番短い辺を100pxにするようにリサイズ(アスペクト比を保つ)
+# 100×100#	アスペクト比を保ち一番短い辺を100pxにするようにリサイズし、画像を中央によせ、はみ出た部分は切り取る
 end
